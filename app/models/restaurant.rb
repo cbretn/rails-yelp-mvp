a762f854aa0b0c91bfb7fda8b83698a6e564f6e1
Charles Breton <charles.bretn@gmail.com>
2019-02-09 12:27:07 +0800

    restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
+# A restaurant has a name, an address, a category and a phone_number
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurant
+  resources :restaurants
 end
