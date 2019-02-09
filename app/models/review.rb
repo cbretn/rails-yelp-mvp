957aea6ac5542fd5c8bedacaef37183bf230c1c1
Charles Breton <charles.bretn@gmail.com>
2019-02-09 16:24:13 +0800

    added review model, migration ran (incl. env=test

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+# a review belongs to a restaurant, and has a content and a rating from 1 to 5.
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :ration, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
